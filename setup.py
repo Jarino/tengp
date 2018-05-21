8acf4f8a79dbe5a715c833020d3948bf1e2155b4
jarino <jaroslavloebl@gmail.com>
2018-05-21 13:48:43 +0200

    added setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='pycgp',
+    version='0.1',
+    description='CGP library built on NumPy and TensorFlow',
+    author='Jaroslav Loebl',
+    author_email='jaroslavloebl@gmail.com',
+    packages=['pycgp']
+)
