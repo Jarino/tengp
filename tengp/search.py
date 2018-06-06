5433724533f3b65f23e717d11f4da94c1d0100b1
Jaroslav Loebl <jaroslavloebl@gmail.com>
2018-06-07 00:59:30 +0200

    prototype of tabu search algorithm also changed the iris classification notebook, for testing purposes

--- tengp/search.py
+++ tengp/search.py
@@ -1,3 +1,4 @@
+from collections import deque
 import random
 
 from .mutations import point_mutation
@@ -38,7 +39,7 @@ def evolution_strategy(X, y, cost_function, params,
         if parent.fitness <= target_fitness:
             return population
         
-        population = [point_mutation(parent) for _ in range(population_size - 1)]
+        population = [parent.apply(point_mutation(parent)) for _ in range(population_size - 1)]
 
         population += [parent]
 
@@ -53,6 +54,72 @@ def evolution_strategy(X, y, cost_function, params,
     return population
 
 
-    
+def tabu_search(X, y, cost_function, params,
+        target_fitness=0,
+        evaluations=5000,
+        memory_size=10,
+        random_state=None,
+        verbose=True):
+        
+        same = 0
+        cycles = 0
+
+        if random_state:
+            random.seed(random_state)
+
+        n_evals = 0
+        memory = deque(maxlen=memory_size)
+
+        ib = IndividualBuilder(params)
+
+        individual = ib.create()
+
+        output = individual.transform(X)
+        individual.fitness = cost_function(y, output)
+        n_evals += 1
+
+
+        while n_evals < evaluations:
+            if verbose and n_evals % 500 == 0:
+                print(f'N_evals: {n_evals}, fitness: {individual.fitness}')
+
+            # generate moves
+            moves = [point_mutation(individual) for _ in range(5)]
+            
+            children = []
+
+            for move in moves:
+                child = individual.apply(move)
+                if child == individual:
+                    child.fitness = individual.fitness
+                else:
+                    output = child.transform(X)
+                    child.fitness = cost_function(y, output)
+                    n_evals += 1
+                children.append((child, move))
+
+            children.sort(key=lambda x: x[0].fitness)
+            
+            best = children[0]
+            
+            if best[0] in memory:
+                same += 1
+                continue
+
+            individual = best[0]
+            memory.append(best[0])
+
+
+
+
+        return individual, same, cycles
+        
+
+
+
+
+
+
+
 
     
