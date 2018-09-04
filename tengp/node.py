cd909f7ed8e65e5fc63205ed693b9964dfc9c55c
jarino <jaroslavloebl@gmail.com>
2018-09-04 17:08:02 +0200

    lol wut

--- tengp/node.py
+++ tengp/node.py
@@ -21,8 +21,5 @@ class Node():
         return f'{name}, {self.inputs}'
 
 class RealValuedNode(Node):
-    def evaluate(self):
-        print('no nech ti nejebe')
-
     def __repr__(self):
         return f'{self.fun}, {self.inputs}'
