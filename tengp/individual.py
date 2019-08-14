5a23be967b47f3a846e685c70740318d18eef56b
Jaroslav Loebl <jaroslavloebl@gmail.com>
2019-08-14 12:10:49 +0200

    remove unused TFIndividual

--- tengp/individual.py
+++ tengp/individual.py
@@ -189,36 +189,6 @@ class NPIndividual(Individual):
         else:
             return np.array(output).T
 
-class TFIndividual(Individual):
-
-    def __init__(self, genes, bounds, params):
-        self.nodes = map_to_tf_phenotype(genes, params)
-        Individual.__init__(self, genes, bounds, params)
-
-    def transform(self, X):
-        for path in self.paths:
-            for index in path:
-                current_node = self.nodes[index]
-
-                if current_node.fun.__name__ == 'constant': # quite shitty way to check
-                    current_node.value = current_node.fun(X[:, index])
-                elif current_node.fun.__name__ == 'Variable':
-                    input_index = current_node.inputs[0]
-                    initial_value = self.nodes[input_index].value 
-                    current_node.value = current_node.fun(initial_value)
-                else:
-                    values = [self.nodes[i].value for i in current_node.inputs[:current_node.arity]]
-                    current_node.value = current_node.fun(*values)
-
-
-        with tf.Session() as sess:
-            sess.run(tf.global_variables_initializer())
-            output = []
-            for i in range(1, self.params.n_outputs + 1):
-                output.append(self.nodes[-i].value.eval())
-
-        return output
-
 
 class IndividualBuilder():
     def __init__(self, params):
