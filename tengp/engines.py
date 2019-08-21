2fe730c7007565afd894c945f44042843b39e845
Jaroslav Loebl <jaroslavloebl@gmail.com>
2019-08-21 10:00:48 +0200

    reimplementation of real-valued evaluation of individual

--- tengp/engines.py
+++ tengp/engines.py
@@ -5,6 +5,86 @@ from sympy import Symbol, lambdify
 
 from .parameters import Parameters
 
+class RealValuedEngine():
+    def __init__(self, params: Parameters):
+        self.params = params
+        self.n_inputs = params.n_inputs
+        self.arity = params.function_set.max_arity
+        self.function_set = params.function_set
+
+    def execute(self, genotype, X):
+        # genotype is a simple list of real values
+        exec_stack = []
+        exec_path = set()
+        expr_dict = {}
+
+        # handle the output genes
+        for gene in genotype[-self.params.n_outputs:]:
+            lower = math.floor(gene)
+            upper = math.ceil(gene)
+
+            exec_stack.append(lower)
+            exec_path.add(lower)
+
+            if lower != upper:
+                exec_stack.append(upper)
+                exec_path.add(upper)
+
+        while len(exec_stack) > 0:
+            node_index = exec_stack.pop()
+
+            if node_index < self.n_inputs:
+                continue
+
+            start_gene_index = (node_index - self.n_inputs)*(self.arity+1)
+            node_genes = genotype[start_gene_index:start_gene_index+(self.arity+1)]
+
+            # exclude the function gene
+            for gene in reversed(node_genes[1:]):
+                lower = math.floor(gene)
+                upper = math.ceil(gene)
+                
+                if lower not in exec_path:
+                    exec_stack.append(lower)
+                    exec_path.add(lower)
+
+                if lower != upper:
+                    if upper not in exec_path:
+                        exec_stack.append(upper)
+                        exec_path.add(upper)
+
+
+        for node_index in sorted(list(exec_path)):
+            # input nodes
+            if node_index < self.n_inputs:
+                expr_dict[node_index] = X[:, node_index]
+                continue
+
+            # function nodes here
+            start_gene_index = (node_index - self.n_inputs)*(self.arity+1)
+            node_inputs = genotype[start_gene_index+1:start_gene_index+1+self.arity]
+
+            inputs = []
+            for i in node_inputs:#[:function_arity]:
+                coeff = i - int(i)
+                L = (1-coeff)*expr_dict[math.floor(i)]
+                R = coeff*expr_dict[math.ceil(i)]
+
+                inputs.append(L + R)
+
+            fun_coeff = start_gene_index - int(start_gene_index)
+
+            l_fun, _ = self.function_set[math.floor(start_gene_index)]
+            u_fun, _ = self.function_set[math.ceil(start_gene_index)]
+
+            expr_dict[node_index] = (1-fun_coeff)*l_fun(*inputs) + fun_coeff*u_fun(*inputs)
+
+        coeff = genotype[-1] - int(genotype[-1])
+        L = (1-coeff)*expr_dict[math.floor(genotype[-1])]
+        R = coeff*expr_dict[math.ceil(genotype[-1])]
+        final_exp = L + R
+        return final_exp
+
 
 class FixedFunctionRowEngine():
     def __init__(self, params: Parameters):
@@ -22,7 +102,6 @@ class FixedFunctionRowEngine():
         # genotype is a simple list of real values
         exec_stack = []
         exec_path = set()
-        input_symbols = [Symbol(f'i{i}') for i in range(self.params.n_inputs)]
         expr_dict = {}
 
         # handle the output genes
