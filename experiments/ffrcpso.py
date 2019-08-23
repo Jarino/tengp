3f6459f620d3d632424680aa8a8e2cbe66dc77d0
Jaroslav Loebl <jaroslavloebl@gmail.com>
2019-08-23 16:26:31 +0200

    FFRCPSO max_back=1, (0,1)

--- experiments/ffrcpso.py
+++ experiments/ffrcpso.py
@@ -66,10 +66,10 @@ def main():
             n_rows=len(funset),
             n_columns=50//len(funset),
             function_set=funset,
-            max_back=20
+            max_back=1
         )
 
-    factory = tengp.FFRCoeffGenotypeFactory(params, -1, 1) 
+    factory = tengp.FFRCoeffGenotypeFactory(params, 0, 1) 
     engine = tengp.FFRCoeffEngine(params)
 
     pg.set_global_rng_seed(seed=42)
